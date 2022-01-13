19bce4996d78c46d8b3566e148dd951eb462f624
anoushka-61 <anushkasrivastava083@gmail.com>
2022-01-13 20:24:21 +0530

    fourth commit

--- src/components/About.js
+++ src/components/About.js
@@ -35,31 +35,31 @@ export default function About() {
     <div className="accordion-item"style={mystyle}>
         <h2 className="accordion-header" id="flush-headingOne"style={mystyle}>
         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne"style={mystyle}>
-            Accordion Item #1
+            About Developer
         </button>
         </h2>
         <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample"style={mystyle}>
-        <div className="accordion-body"style={mystyle}>Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the first item's accordion body.</div>
+        <div className="accordion-body"style={mystyle}>TextUtils is created by Anoushka Srivastava , B-Tech CSE,3rd Year student.
         </div>
     </div>
     <div className="accordion-item"style={mystyle}>
         <h2 className="accordion-header" id="flush-headingTwo"style={mystyle}>
         <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo"style={mystyle}>
-            Accordion Item #2
+         TextUtils-Background
         </button>
         </h2>
         <div style={mystyle} id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-        <div style={mystyle} className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+        <div style={mystyle} className="accordion-body">TextUtils is a react app </div>
         </div>
     </div>
     <div style={mystyle} className="accordion-item">
         <h2  style={mystyle} className="accordion-header" id="flush-headingThree">
         <button style={mystyle} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-            Accordion Item #3
+           TextUtils-Features
         </button>
         </h2>
         <div style={mystyle} id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-        <div style={mystyle} className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> className. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+        <div style={mystyle} className="accordion-body">TextUtils is a web app which can be used for manipulating your texts. This can change text's cases & copy and paste  the text. You can count the number of words and characters with the preview shown below.</div>
         </div>
     </div>
     </div>
